dfdcadc5ee48c1e43521d2140be3424e4bbc9d59
diegoparradoc96 <diegoparrado.cardozo@gmail.com>
2025-09-05 14:27:58 -0500

    progress/adding-players

--- src/context/PlayerContext.tsx
+++ src/context/PlayerContext.tsx
@@ -1,36 +1,61 @@
-import React, {createContext, useContext, useState} from 'react';
-import type {ReactNode} from "react";
+import React, { createContext, useContext, useState } from "react";
+import type { ReactNode } from "react";
 
 /* types */
-import type {IPlayer} from "../types";
+import type { IPlayer } from "../types";
 
 interface IPlayerContext {
   player: IPlayer;
+  arrPlayers: IPlayer[];
+  selectPlayer: (player: IPlayer) => void;
   addPlayer: (player: IPlayer) => void;
+  removePlayer: (player: IPlayer) => void;
   clearPlayer: () => void;
 }
 
 const PlayerContext = createContext<IPlayerContext | undefined>(undefined);
 
 const PlayerProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [player, setPlayer] = useState<IPlayer>({ name: "", class: {    
+  const [player, setPlayer] = useState<IPlayer>({
     name: "",
-    spec: {
+    class: {
       name: "",
-      image: ""
-    }
-  } });
+      spec: {
+        name: "",
+        image: "",
+      },
+    },
+  });
+  const [arrPlayers, setArrPlayers] = useState<IPlayer[]>([]);
+
+  const selectPlayer = (newPlayer: IPlayer) => {
+    setPlayer(newPlayer);
+  };
 
   const addPlayer = (newPlayer: IPlayer) => {
-    setPlayer(newPlayer);
-  }
+    setArrPlayers((prev) => [...prev, newPlayer]);
+  };
+
+  /* debo remover por id no por nombre */
+  const removePlayer = (player: IPlayer) => {
+    setArrPlayers((prev) => prev.filter((p) => p.name !== player.name));
+  };
 
   const clearPlayer = () => {
     setPlayer({ name: "", class: { name: "", spec: { name: "", image: "" } } });
   };
 
   return (
-    <PlayerContext.Provider value={{ player, addPlayer, clearPlayer }}>
+    <PlayerContext.Provider
+      value={{
+        player,
+        selectPlayer,
+        arrPlayers,
+        addPlayer,
+        removePlayer,
+        clearPlayer,
+      }}
+    >
       {children}
     </PlayerContext.Provider>
   );
