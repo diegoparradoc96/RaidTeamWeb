90f87ecc8ac7ebd684a16a419d375d85b7cdef03
diegoparradoc96 <diegoparrado.cardozo@gmail.com>
2025-09-30 02:26:36 -0500

    firts deploy

---
+++ src/utils/imageImports.ts
@@ -0,0 +1,76 @@
+// Image imports
+import warriorArmsImg from '../assets/Warrior_Arms.PNG';
+import warriorProtImg from '../assets/Warrior_Prot.PNG';
+import warriorFuryImg from '../assets/Warrior_Fury.PNG';
+import mageArcaneImg from '../assets/Mage_Arcane.PNG';
+import mageFireImg from '../assets/Mage_Fire.PNG';
+import mageFrostImg from '../assets/Mage_Frost.PNG';
+import rogueAssassinationImg from '../assets/Rogue_Assassination.PNG';
+import rogueCombatImg from '../assets/Rogue_Combat.PNG';
+import rogueSubtletyImg from '../assets/Rogue_Subtlety.PNG';
+import priestDisciplineImg from '../assets/Priest_Discipline.PNG';
+import priestHolyImg from '../assets/Priest_Holy.PNG';
+import priestShadowImg from '../assets/Priest_Shadow.PNG';
+import hunterBMImg from '../assets/Hunter_BM.PNG';
+import hunterMMImg from '../assets/Hunter_MM.PNG';
+import hunterSurvivalImg from '../assets/Hunter_Survival.PNG';
+import shamanElementalImg from '../assets/Shaman_Elemental.PNG';
+import shamanEnhaImg from '../assets/Shaman_Enha.PNG';
+import shamanRestoImg from '../assets/Shaman_Resto.PNG';
+import paladinHolyImg from '../assets/Paladin_Holy.PNG';
+import paladinProtImg from '../assets/Paladin_Prot.PNG';
+import paladinRetriImg from '../assets/Paladin_Retri.PNG';
+import warlockAfflictionImg from '../assets/Warlock_Affliction.PNG';
+import warlockDemonologyImg from '../assets/Warlock_Demonology.PNG';
+import warlockDestructionImg from '../assets/Warlock_Destruction.PNG';
+import druidBalanceImg from '../assets/Druid_Balance.PNG';
+import druidFeralImg from '../assets/Druid_Feral.PNG';
+import druidRestoImg from '../assets/Druid_Resto.PNG';
+
+export const ClassImages = {
+  Warrior: {
+    Arms: warriorArmsImg,
+    Prot: warriorProtImg,
+    Fury: warriorFuryImg
+  },
+  Mage: {
+    Arcane: mageArcaneImg,
+    Fire: mageFireImg,
+    Frost: mageFrostImg
+  },
+  Rogue: {
+    Assassination: rogueAssassinationImg,
+    Combat: rogueCombatImg,
+    Subtlety: rogueSubtletyImg
+  },
+  Priest: {
+    Discipline: priestDisciplineImg,
+    Holy: priestHolyImg,
+    Shadow: priestShadowImg
+  },
+  Hunter: {
+    BeastMastery: hunterBMImg,
+    Marksmanship: hunterMMImg,
+    Survival: hunterSurvivalImg
+  },
+  Shaman: {
+    Elemental: shamanElementalImg,
+    Enhancement: shamanEnhaImg,
+    Restoration: shamanRestoImg
+  },
+  Paladin: {
+    Holy: paladinHolyImg,
+    Protection: paladinProtImg,
+    Retribution: paladinRetriImg
+  },
+  Warlock: {
+    Affliction: warlockAfflictionImg,
+    Demonology: warlockDemonologyImg,
+    Destruction: warlockDestructionImg
+  },
+  Druid: {
+    Balance: druidBalanceImg,
+    Feral: druidFeralImg,
+    Restoration: druidRestoImg
+  }
+};
